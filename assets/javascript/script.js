393a3591d4c0fa17dd514e968eafd5643c12488b
Pearl <gansuvd777@gmail.com>
2018-09-20 03:00:18 -0500

    last touch up for adding audio

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -112,81 +112,113 @@ document.onkeypress = function(event) {
 var play = function () {
 
 	if (currentWord === "rich") {
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/youcandoit.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars1.githubusercontent.com/u/42152762?s=200&v=4'  alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	} 
 	else if (currentWord === "elizabeth"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/2.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars1.githubusercontent.com/u/42365244?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "magaly"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/focus.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars2.githubusercontent.com/u/25308742?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "joyce"){
-		var picture = "<img src='https://avatars1.githubusercontent.com/u/42484077?s=200&v=4' alt=''>";
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/thinking.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
+		var picture = "<img src='https://avatars2.githubusercontent.com/u/25308742?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "andrew"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/icantbelieve.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars2.githubusercontent.com/u/32251290?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "ashley"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/3.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars3.githubusercontent.com/u/38448889?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "ryan"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/woohoo.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars0.githubusercontent.com/u/38480369?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "abigal"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/hey-sweetness-1.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars0.githubusercontent.com/u/41799687?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "jason"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/gotit1.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars3.githubusercontent.com/u/40837639?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "isaac"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/im-the-man.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars3.githubusercontent.com/u/727268?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "pearl"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/its-almost-time.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars0.githubusercontent.com/u/39831295?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "andy"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/gotit2.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars1.githubusercontent.com/u/10361698?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "angelo"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/powerful.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars3.githubusercontent.com/u/727268?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "brett"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/1.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars3.githubusercontent.com/u/727268?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "katie"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/okay-well.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars3.githubusercontent.com/u/38897241?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
 	}
 	else if (currentWord === "tyler"){
+		var audio = "<audio autoplay = 'autoplay' >" + "<source src='assets/audio/nice-work.wav' type='audio/mpeg'></audio>";
+		document.querySelector("#audio").innerHTML = audio;
 		var picture = "<img src='https://avatars2.githubusercontent.com/u/42359367?s=200&v=4' alt=''>";
 		document.querySelector("#picture").innerHTML = picture;
 		start();
